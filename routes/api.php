29632b64015cf86e865ddec6328b1c9dfb2fa54e
Anthony <anthony.zink@outlook.fr>
2023-04-30 19:33:14 +0200

    Seed + test api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/users', function (Request $request) {
+    return User::all();
 });
